7df05f31430cc8bd1afe45420617afabd0cb249f
Keenan Woodall <keenanwoodall@gmail.com>
2019-02-08 13:37:00 -0600

    Created UV Scale deformer.

---
+++ Code/Editor/Mesh/Deformers/UVScaleDeformerEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Deform;
+
+namespace DeformEditor
+{
+	[CustomEditor (typeof (UVScaleDeformer)), CanEditMultipleObjects]
+	public class UVScaleDeformerEditor : DeformerEditor
+	{
+		private class Content
+		{
+			public static readonly GUIContent Factor = DeformEditorGUIUtility.DefaultContent.Factor;
+			public static readonly GUIContent Scale = new GUIContent (text: "Scale");
+		}
+
+		private class Properties
+		{
+			public SerializedProperty Factor;
+			public SerializedProperty Scale;
+
+			public Properties (SerializedObject obj)
+			{
+				Factor = obj.FindProperty ("factor");
+				Scale = obj.FindProperty ("scale");
+			}
+		}
+
+		private Properties properties;
+
+		protected override void OnEnable ()
+		{
+			base.OnEnable ();
+			properties = new Properties (serializedObject);
+		}
+
+		public override void OnInspectorGUI ()
+		{
+			base.OnInspectorGUI ();
+
+			serializedObject.UpdateIfRequiredOrScript ();
+
+			EditorGUILayout.PropertyField (properties.Factor, Content.Factor);
+			EditorGUILayout.PropertyField (properties.Scale, Content.Scale);
+
+			serializedObject.ApplyModifiedProperties ();
+
+			EditorApplication.QueuePlayerLoopUpdate ();
+		}
+	}
+}
---
+++ Code/Runtime/Mesh/Deformers/UVScaleDeformer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Mathematics;
+
+namespace Deform
+{
+	[Deformer (Name = "UV Scale", Description = "Scales the mesh's UVs", Type = typeof (UVScaleDeformer), Category = Category.Normal)]
+	public class UVScaleDeformer : Deformer, IFactor
+	{
+		public float Factor
+		{
+			get => factor;
+			set => factor = value;
+		}
+		public Vector2 Scale
+		{
+			get => scale;
+			set => scale = value;
+		}
+
+		[SerializeField, HideInInspector] private float factor = 1f;
+		[SerializeField, HideInInspector] private Vector2 scale = Vector2.one;
+
+		public override DataFlags DataFlags => DataFlags.UVs;
+
+		public override JobHandle Process (MeshData data, JobHandle dependency = default)
+		{
+			return new UVScaleJob
+			{
+				scale = Scale * Factor,
+				uvs = data.DynamicNative.UVBuffer
+			}.Schedule (data.Length, BatchCount, dependency);
+		}
+
+		[BurstCompile (CompileSynchronously = COMPILE_SYNCHRONOUSLY)]
+		private struct UVScaleJob : IJobParallelFor
+		{
+			public float2 scale;
+			public NativeArray<float2> uvs;
+
+			public void Execute (int index)
+			{
+				uvs[index] *= scale;
+			}
+		}
+	}
+}
