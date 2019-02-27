33a6f45eb132f68d8e445af947bc9067a98b64e7
keenanwoodall <keenanwoodall@gmail.com>
2019-02-27 02:31:26 -0600

    Added menu item to clean all deformables

---
+++ Code/Editor/MenuItemActions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Deform;
+
+namespace DeformEditor
+{
+	public static class MenuItemActions
+	{
+		[MenuItem ("Tools/Deform/Actions/Clean All Deformables In Scene", priority = 10100)]
+		public static void CleanDeformablesInScene ()
+		{
+			var deformables = GameObject.FindObjectsOfType<Deformable> ();
+
+			Undo.RecordObjects (deformables, "Cleaned All Deformables In Scene");
+			foreach (var deformable in deformables)
+				deformable.DeformerElements.RemoveAll (t => t.Component == null);
+		}
+	}
+}
