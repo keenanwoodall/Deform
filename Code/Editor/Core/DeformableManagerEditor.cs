f2f19d1e2fd91b4c1c39de8e33d029077989b297
keenanwoodall <keenanwoodall@gmail.com>
2019-02-27 01:43:54 -0600

    Created custom editor for deformable manager

---
+++ Code/Editor/Core/DeformableManagerEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Deform;
+
+namespace DeformEditor
+{
+	[CustomEditor (typeof (DeformableManager)), CanEditMultipleObjects]
+	public class DeformableManagerEditor : Editor
+	{
+		private class Content
+		{
+			public static readonly GUIContent Update = new GUIContent (text: "Update");
+		}
+
+		private class Properties
+		{
+			public SerializedProperty Update;
+
+			public Properties (SerializedObject obj)
+			{
+				Update = obj.FindProperty ("update");
+			}
+		}
+
+		private static bool ShowDebug;
+
+		private Properties properties;
+
+		private void OnEnable ()
+		{
+			properties = new Properties (serializedObject);
+		}
+
+		public override void OnInspectorGUI ()
+		{
+			serializedObject.UpdateIfRequiredOrScript ();
+
+			EditorGUILayout.PropertyField (properties.Update, Content.Update);
+
+			serializedObject.ApplyModifiedProperties ();
+		}
+	}
+}
