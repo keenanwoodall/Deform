cdb01a1969fbcf1c5c3866705ac171233f86e935
keenanwoodall <keenanwoodall@gmail.com>
2019-03-23 15:04:05 -0500

    Changed default deform editor settings

--- Code/Editor/DeformEditorSettingsAsset.cs
+++ Code/Editor/DeformEditorSettingsAsset.cs
@@ -10,10 +10,10 @@ namespace DeformEditor
 #pragma warning restore
 
 		[Header ("Scene")]
-		public Color solidHandleColor = new Color (0f, 1f, 0f);
-		public Color lightHandleColor = new Color (0f, 1f, 0f, 0.5f);
+		public Color solidHandleColor = new Color (1f, 0.4f, 0f, 1f);
+		public Color lightHandleColor = new Color (1f, 0.4f, 0f, 0.5f);
 		public float dottedLineSize = 5f;
-		public float screenspaceHandleCapSize = 0.25f;
+		public float screenspaceHandleCapSize = 0.0275f;
 		public float screenspaceAngleHandleSize = 1.25f;
 	}
 }
