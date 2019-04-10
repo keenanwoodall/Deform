7ecfd406345c07f5d0c84a0b68377e5a81639c49
keenanwoodall <keenanwoodall@gmail.com>
2019-04-09 20:50:52 -0500

    Created recalculate bounds and normals deformers

---
+++ Code/Runtime/Mesh/Deformers/Utility/RecalculateBoundsDeformer.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Jobs;
+
+namespace Deform
+{
+	[Deformer (Name = "Recalculate Bounds", Description = "Recalculates the bounds", Type = typeof (RecalculateBoundsDeformer), Category = Category.Utility)]
+	public class RecalculateBoundsDeformer : Deformer
+	{
+		public override DataFlags DataFlags => DataFlags.Bounds;
+
+		public override JobHandle Process (MeshData data, JobHandle dependency = default)
+		{
+			return MeshUtils.RecalculateBounds (data.DynamicNative, dependency);
+		}
+	}
+}
---
+++ Code/Runtime/Mesh/Deformers/Utility/RecalculateNormalsDeformer.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Jobs;
+
+namespace Deform
+{
+	[Deformer (Name = "Recalculate Normals", Description = "Recalculates the normals", Type = typeof (RecalculateNormalsDeformer), Category = Category.Utility)]
+	public class RecalculateNormalsDeformer : Deformer
+	{
+		public override DataFlags DataFlags => DataFlags.Normals;
+
+		public override JobHandle Process (MeshData data, JobHandle dependency = default)
+		{
+			return MeshUtils.RecalculateNormals (data.DynamicNative, dependency);
+		}
+	}
+}
