7fb37e45fa14aabf7fe44d3e50e8e95dd8e4d7aa
Brullworfel <alexey.mahdi@gmail.com>
2019-12-08 12:33:47 +0200

    Add Factor property to TransformDeformer

--- Code/Runtime/Mesh/Deformers/TransformDeformer.cs
+++ Code/Runtime/Mesh/Deformers/TransformDeformer.cs
@@ -9,7 +9,7 @@ namespace Deform
 {
 	[Deformer (Name = "Transform", Description = "Gives the mesh a new position, rotation and scale", Type = typeof (TransformDeformer))]
     [HelpURL("https://github.com/keenanwoodall/Deform/wiki/TransformDeformer")]
-    public class TransformDeformer : Deformer
+    public class TransformDeformer : Deformer, IFactor
 	{
 		public Transform Target
 		{
@@ -21,16 +21,32 @@ namespace Deform
 			}
 			set => target = value;
 		}
+		public float Factor
+		{
+			get => factor;
+			set => factor = value;
+		}
 
 		[SerializeField, HideInInspector]
 		private Transform target;
+		[SerializeField, HideInInspector]
+		private float factor = 1f;
 
 		public override DataFlags DataFlags => DataFlags.Vertices;
 
 		public override JobHandle Process (MeshData data, JobHandle dependency = default)
 		{
+			Factor = Mathf.Clamp(Factor, 0f, 1f);
+			if (Factor == 0f)
+				return dependency;
 			var dataTargetTransform = data.Target.GetTransform ();
-			var matrix = dataTargetTransform.worldToLocalMatrix * Target.localToWorldMatrix;
+			Matrix4x4 matrix = new Matrix4x4();
+			matrix.SetTRS(
+				Vector3.Lerp(dataTargetTransform.position, Target.position, Factor),
+				Quaternion.Lerp(dataTargetTransform.rotation, Target.rotation, Factor),
+				Vector3.Lerp(dataTargetTransform.localScale, Target.localScale, Factor)
+				);
+			matrix = dataTargetTransform.worldToLocalMatrix * matrix;
 			return new TransformJob
 			{
 				matrix = matrix,
