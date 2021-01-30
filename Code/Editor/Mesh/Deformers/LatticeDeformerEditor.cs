bef131a8bcfa50a1be7a60efd4ce8609ed731402
Barnaby MacBook <barnaby@sabresaurus.com>
2021-01-30 15:35:11 +0000

    * Lattice deformer now supports subdivision resolution

--- Code/Editor/Mesh/Deformers/LatticeDeformerEditor.cs
+++ Code/Editor/Mesh/Deformers/LatticeDeformerEditor.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.IMGUI.Controls;
-using Beans.Unity.Editor;
 using Deform;
 
 namespace DeformEditor
@@ -9,18 +7,23 @@ namespace DeformEditor
     [CustomEditor(typeof(LatticeDeformer)), CanEditMultipleObjects]
     public class LatticeDeformerEditor : DeformerEditor
     {
+        private Vector3Int newResolution;
+
         private static class Content
         {
             public static readonly GUIContent Corners = new GUIContent(text: "Corners", tooltip: "The lattice control points");
+            public static readonly GUIContent Resolution = new GUIContent(text: "Resolution", tooltip: "Per axis control point counts, the higher the resolution the more splits");
         }
 
         private class Properties
         {
             public SerializedProperty Corners;
+            public SerializedProperty Resolution;
 
             public Properties(SerializedObject obj)
             {
                 Corners = obj.FindProperty("corners");
+                Resolution = obj.FindProperty("resolution");
             }
         }
 
@@ -31,6 +34,7 @@ namespace DeformEditor
             base.OnEnable();
 
             properties = new Properties(serializedObject);
+            newResolution = properties.Resolution.vector3IntValue;
         }
 
         public override void OnInspectorGUI()
@@ -40,6 +44,15 @@ namespace DeformEditor
             serializedObject.UpdateIfRequiredOrScript();
 
             EditorGUILayout.PropertyField(properties.Corners, Content.Corners, true);
+
+            newResolution = EditorGUILayout.Vector3IntField(Content.Resolution, newResolution);
+            // Make sure we have at least two control points per axis
+            newResolution = Vector3Int.Max(newResolution, new Vector3Int(2, 2, 2));
+            if (GUILayout.Button("Update"))
+            {
+                Undo.RecordObject(target, "Update Lattice");
+                ((LatticeDeformer) target).GenerateCorners(newResolution);
+            }
 
             serializedObject.ApplyModifiedProperties();
 
@@ -54,20 +67,44 @@ namespace DeformEditor
 
             Handles.matrix = lattice.transform.localToWorldMatrix;
             var corners = lattice.Corners;
-            DeformHandles.Line(corners[0], corners[1], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[1], corners[3], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[2], corners[3], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[2], corners[0], DeformHandles.LineMode.SolidDotted);
 
-            DeformHandles.Line(corners[4], corners[5], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[5], corners[7], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[6], corners[7], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[6], corners[4], DeformHandles.LineMode.SolidDotted);
+            var resolution = lattice.Resolution;
+            for (int z = 0; z < resolution.z - 1; z++)
+            {
+                for (int y = 0; y < resolution.y - 1; y++)
+                {
+                    for (int x = 0; x < resolution.x - 1; x++)
+                    {
+                        int index000 = lattice.GetIndex(x, y, z);
+                        int index100 = lattice.GetIndex(x + 1, y, z);
+                        int index010 = lattice.GetIndex(x, y + 1, z);
+                        int index110 = lattice.GetIndex(x + 1, y + 1, z);
+                        int index001 = lattice.GetIndex(x, y, z + 1);
+                        int index101 = lattice.GetIndex(x + 1, y, z + 1);
+                        int index011 = lattice.GetIndex(x, y + 1, z + 1);
+                        int index111 = lattice.GetIndex(x + 1, y + 1, z + 1);
 
-            DeformHandles.Line(corners[0], corners[4], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[1], corners[5], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[2], corners[6], DeformHandles.LineMode.SolidDotted);
-            DeformHandles.Line(corners[3], corners[7], DeformHandles.LineMode.SolidDotted);
+                        var lineMode = DeformHandles.LineMode.Solid;
+                        DeformHandles.Line(corners[index000], corners[index100], lineMode);
+                        DeformHandles.Line(corners[index010], corners[index110], lineMode);
+                        DeformHandles.Line(corners[index001], corners[index101], lineMode);
+                        DeformHandles.Line(corners[index011], corners[index111], lineMode);
+
+                        DeformHandles.Line(corners[index000], corners[index010], lineMode);
+                        DeformHandles.Line(corners[index100], corners[index110], lineMode);
+                        DeformHandles.Line(corners[index001], corners[index011], lineMode);
+                        DeformHandles.Line(corners[index101], corners[index111], lineMode);
+
+                        DeformHandles.Line(corners[index000], corners[index001], lineMode);
+                        DeformHandles.Line(corners[index100], corners[index101], lineMode);
+                        DeformHandles.Line(corners[index010], corners[index011], lineMode);
+                        DeformHandles.Line(corners[index110], corners[index111], lineMode);
+
+
+                        //Handles.SphereHandleCap(0, corners[index], Quaternion.identity, 0.1f, EventType.Repaint);
+                    }
+                }
+            }
 
             EditorApplication.QueuePlayerLoopUpdate();
         }
--- Code/Runtime/Mesh/Deformers/LatticeDeformer.cs
+++ Code/Runtime/Mesh/Deformers/LatticeDeformer.cs
@@ -4,6 +4,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using static Unity.Mathematics.math;
+using float3 = Unity.Mathematics.float3;
 
 namespace Deform
 {
@@ -15,6 +16,12 @@ namespace Deform
         {
             get => corners;
             set => corners = value;
+        }
+
+        public Vector3Int Resolution
+        {
+            get => resolution;
+            set => resolution = value;
         }
 
         [SerializeField] private float3[] corners =
@@ -30,6 +37,34 @@ namespace Deform
             0.5f * new float3(1, 1, 1),
         };
 
+        [SerializeField] private Vector3Int resolution = new Vector3Int(2, 2, 2);
+
+        public void GenerateCorners(Vector3Int newResolution)
+        {
+            resolution = newResolution;
+            corners = new float3[resolution.x * resolution.y * resolution.z];
+
+            for (int z = 0; z < resolution.z; z++)
+            {
+                for (int y = 0; y < resolution.y; y++)
+                {
+                    for (int x = 0; x < resolution.x; x++)
+                    {
+                        int index = GetIndex(x, y, z);
+
+                        // TODO - Clean up these calculations
+                        corners[index] = 0.5f * new float3(Mathf.Lerp(-1, 1, x / (float) (resolution.x - 1)),
+                            Mathf.Lerp(-1, 1, y / (float) (resolution.y - 1)), Mathf.Lerp(-1, 1, z / (float) (resolution.z - 1)));
+                    }
+                }
+            }
+        }
+
+        public int GetIndex(int x, int y, int z)
+        {
+            return x + y * resolution.x + z * (resolution.x * resolution.y);
+        }
+
         public override DataFlags DataFlags => DataFlags.Vertices;
 
         public override JobHandle Process(MeshData data, JobHandle dependency = default)
@@ -37,6 +72,7 @@ namespace Deform
             return new LatticeJob
             {
                 corners = new NativeArray<float3>(corners, Allocator.TempJob),
+                resolution = new int3(resolution.x, resolution.y, resolution.z),
                 vertices = data.DynamicNative.VertexBuffer
             }.Schedule(data.Length, DEFAULT_BATCH_COUNT, dependency);
         }
@@ -45,61 +81,81 @@ namespace Deform
         public struct LatticeJob : IJobParallelFor
         {
             [DeallocateOnJobCompletion, ReadOnly] public NativeArray<float3> corners;
+            [ReadOnly] public int3 resolution;
             public NativeArray<float3> vertices;
 
+            // TODO -  Rewrite these calculations to be simpler now that the maths is figured out
             public void Execute(int index)
             {
                 // Convert from [-0.5,0.5] space to [0,1]
                 var sourcePosition = vertices[index] + float3(0.5f, 0.5f, 0.5f);
 
-                var newPosition = vertices[index];
+                // Determine the negative corner of the lattice cell containing the source position
+                var negativeCorner = new int3((int) (sourcePosition.x * (resolution.x - 1)), (int) (sourcePosition.y * (resolution.y - 1)), (int) (sourcePosition.z * (resolution.z - 1)));
+
+                // Clamp the corner to an acceptable range in case the source vertex is outside or on the lattice bounds
+                negativeCorner = max(negativeCorner, new int3(0, 0, 0));
+                negativeCorner = min(negativeCorner, resolution - new int3(2, 2, 2));
+
+                int index0 = (negativeCorner.x + 0) + (negativeCorner.y + 0) * resolution.x + (negativeCorner.z + 0) * (resolution.x * resolution.y);
+                int index1 = (negativeCorner.x + 1) + (negativeCorner.y + 0) * resolution.x + (negativeCorner.z + 0) * (resolution.x * resolution.y);
+                int index2 = (negativeCorner.x + 0) + (negativeCorner.y + 1) * resolution.x + (negativeCorner.z + 0) * (resolution.x * resolution.y);
+                int index3 = (negativeCorner.x + 1) + (negativeCorner.y + 1) * resolution.x + (negativeCorner.z + 0) * (resolution.x * resolution.y);
+                int index4 = (negativeCorner.x + 0) + (negativeCorner.y + 0) * resolution.x + (negativeCorner.z + 1) * (resolution.x * resolution.y);
+                int index5 = (negativeCorner.x + 1) + (negativeCorner.y + 0) * resolution.x + (negativeCorner.z + 1) * (resolution.x * resolution.y);
+                int index6 = (negativeCorner.x + 0) + (negativeCorner.y + 1) * resolution.x + (negativeCorner.z + 1) * (resolution.x * resolution.y);
+                int index7 = (negativeCorner.x + 1) + (negativeCorner.y + 1) * resolution.x + (negativeCorner.z + 1) * (resolution.x * resolution.y);
+
+                var localizedSourcePosition = (sourcePosition) * (resolution - new int3(1,1,1))  - negativeCorner;
+
+                var newPosition = float3.zero;
 
                 {
                     int axisIndex = 0;
                     int secondaryAxisIndex = 1;
                     int tertiaryAxisIndex = 2;
-                    var min1 = lerp(corners[0][axisIndex], corners[1][axisIndex], sourcePosition[secondaryAxisIndex]);
-                    var max1 = lerp(corners[2][axisIndex], corners[3][axisIndex], sourcePosition[secondaryAxisIndex]);
+                    var min1 = lerp(corners[index0][axisIndex], corners[index2][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
+                    var max1 = lerp(corners[index1][axisIndex], corners[index3][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
 
-                    var min2 = lerp(corners[0 + 4][axisIndex], corners[1 + 4][axisIndex], sourcePosition[secondaryAxisIndex]);
-                    var max2 = lerp(corners[2 + 4][axisIndex], corners[3 + 4][axisIndex], sourcePosition[secondaryAxisIndex]);
+                    var min2 = lerp(corners[index4][axisIndex], corners[index6][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
+                    var max2 = lerp(corners[index5][axisIndex], corners[index7][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
 
-                    var min = lerp(min1, min2, sourcePosition[tertiaryAxisIndex]);
-                    var max = lerp(max1, max2, sourcePosition[tertiaryAxisIndex]);
+                    var min = lerp(min1, min2, localizedSourcePosition[tertiaryAxisIndex]);
+                    var max = lerp(max1, max2, localizedSourcePosition[tertiaryAxisIndex]);
 
-                    newPosition[axisIndex] = lerp(min, max, sourcePosition[axisIndex]);
+                    newPosition[axisIndex] = lerp(min, max, localizedSourcePosition[axisIndex]);
                 }
 
                 {
                     int axisIndex = 1;
                     int secondaryAxisIndex = 0;
                     int tertiaryAxisIndex = 2;
-                    var min1 = lerp(corners[0][axisIndex], corners[2][axisIndex], sourcePosition[secondaryAxisIndex]);
-                    var max1 = lerp(corners[1][axisIndex], corners[3][axisIndex], sourcePosition[secondaryAxisIndex]);
+                    var min1 = lerp(corners[index0][axisIndex], corners[index1][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
+                    var max1 = lerp(corners[index2][axisIndex], corners[index3][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
 
-                    var min2 = lerp(corners[0 + 4][axisIndex], corners[2 + 4][axisIndex], sourcePosition[secondaryAxisIndex]);
-                    var max2 = lerp(corners[1 + 4][axisIndex], corners[3 + 4][axisIndex], sourcePosition[secondaryAxisIndex]);
+                    var min2 = lerp(corners[index4][axisIndex], corners[index5][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
+                    var max2 = lerp(corners[index6][axisIndex], corners[index7][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
 
-                    var min = lerp(min1, min2, sourcePosition[tertiaryAxisIndex]);
-                    var max = lerp(max1, max2, sourcePosition[tertiaryAxisIndex]);
+                    var min = lerp(min1, min2, localizedSourcePosition[tertiaryAxisIndex]);
+                    var max = lerp(max1, max2, localizedSourcePosition[tertiaryAxisIndex]);
 
-                    newPosition[axisIndex] = lerp(min, max, sourcePosition[axisIndex]);
+                    newPosition[axisIndex] = lerp(min, max, localizedSourcePosition[axisIndex]);
                 }
 
                 {
                     int axisIndex = 2;
                     int secondaryAxisIndex = 0;
                     int tertiaryAxisIndex = 1;
-                    var min1 = lerp(corners[0][axisIndex], corners[2][axisIndex], sourcePosition[secondaryAxisIndex]);
-                    var max1 = lerp(corners[0 + 4][axisIndex], corners[2 + 4][axisIndex], sourcePosition[secondaryAxisIndex]);
+                    var min1 = lerp(corners[index0][axisIndex], corners[index1][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
+                    var max1 = lerp(corners[index4][axisIndex], corners[index5][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
 
-                    var min2 = lerp(corners[1][axisIndex], corners[3][axisIndex], sourcePosition[secondaryAxisIndex]);
-                    var max2 = lerp(corners[1 + 4][axisIndex], corners[3 + 4][axisIndex], sourcePosition[secondaryAxisIndex]);
+                    var min2 = lerp(corners[index2][axisIndex], corners[index3][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
+                    var max2 = lerp(corners[index6][axisIndex], corners[index7][axisIndex], localizedSourcePosition[secondaryAxisIndex]);
 
-                    var min = lerp(min1, min2, sourcePosition[tertiaryAxisIndex]);
-                    var max = lerp(max1, max2, sourcePosition[tertiaryAxisIndex]);
+                    var min = lerp(min1, min2, localizedSourcePosition[tertiaryAxisIndex]);
+                    var max = lerp(max1, max2, localizedSourcePosition[tertiaryAxisIndex]);
 
-                    newPosition[axisIndex] = lerp(min, max, sourcePosition[axisIndex]);
+                    newPosition[axisIndex] = lerp(min, max, localizedSourcePosition[axisIndex]);
                 }
 
                 vertices[index] = newPosition;
