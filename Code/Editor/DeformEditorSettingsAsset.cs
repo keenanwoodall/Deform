57a023ab838ae37d5393f61c54714e21e1bd163b
Keenan Woodall <keenanwoodall@gmail.com>
2021-12-05 10:39:57 -0600

    Fixed recording color display name

--- Code/Editor/DeformEditorSettingsAsset.cs
+++ Code/Editor/DeformEditorSettingsAsset.cs
@@ -7,8 +7,8 @@ namespace DeformEditor
 	{
 		[CollapsibleSection("Scene")]
 		[DisplayName("Solid Color")] public Color solidHandleColor = new Color (1f, 0.4f, 0f, 1f);
-		[DisplayName("Light Color")] public Color lightHandleColor = new Color (1f, 0.4f, 0f, 0.5f);
-		[DisplayName("Light Color")] public Color recordingHandleColor = new Color (1f, 0f, 0f, 0.9f);
+		[DisplayName("Light Color")] public Color lightHandleColor = new Color (1f, 0.4f, 0f, 0.75f);
+		[DisplayName("Recording Color")] public Color recordingHandleColor = new Color (1f, 0f, 0f, 0.9f);
 		public float dottedLineSize = 5f;
 		[DisplayName("Handle Size")] public float screenspaceHandleCapSize = 0.0275f;
 		[DisplayName("Angle Handle Size")] public float screenspaceAngleHandleSize = 1.25f;
